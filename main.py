226b640187fafec10ffcd4ec05ec5cff464a7d0b
Azam <mazam1488@gmail.com>
2021-10-23 16:11:23 -0400

    req and changes

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from typing import Optional
 from fastapi import FastAPI
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 import aiofiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [""]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
