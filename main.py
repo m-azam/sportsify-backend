f0a2e25bdf20dccf20c1ccc9c41352524367d07f
Excell Pepple <49886472+excellpepple@users.noreply.github.com>
2021-10-24 01:38:38 -0400

    add centered object detection

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import os
 from typing import Optional
+import cv2
 from fastapi import FastAPI
 from fastapi import FastAPI, File, UploadFile, Form
 import aiofiles
 from fastapi.middleware.cors import CORSMiddleware
+from services.object_detect import *
+from modules.gif_generator.generate import *
+import base64
+from PIL import Image
+from PIL.ExifTags import TAGS
+
 
 app = FastAPI()
 
@@ -16,6 +24,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def create_frames(filename):
+    vidcap = cv2.VideoCapture(filename)
+    success,image = vidcap.read()
+    count = 0
+    while count < 1:
+        
+        cv2.imwrite("services/frame%d.jpg" % count, image)     # save frame as JPEG file      
+        success,image = vidcap.read()
+        print('Read a new frame: ', success)
+        count += 1
+
+
+def findImage(filename):
+    create_frames(filename)
+    return detect_object()
+
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -24,5 +50,11 @@ def read_root():
 async def create_upload_file(file: UploadFile = File(...)):
     async with aiofiles.open(file.filename, 'wb') as out_file:
         content = await file.read()  # async read
-        await out_file.write(content)  # async write
-    return {"filename": file.filename}
+        await out_file.write(content)# async write
+        centered_object = findImage(file.filename)
+        
+        
+    return {"centered_object": centered_object.title()}
+
+
+
---
+++ modules/gif_generator/generate.py
@@ -0,0 +1,46 @@
+import cv2
+import imageio
+import os
+import sys
+
+# 1. Get Images
+
+
+# get image from directory
+def get_images(folder):
+    frames = []
+    print(os.listdir(folder))
+    for img in os.listdir(folder):
+        pic = cv2.imread(os.path.join(folder, img))
+        if pic is not None:
+            frames.append(pic)
+            print(f"frame {img} added")
+    return frames
+
+
+
+
+
+def generate_gif():
+    cap = cv2.VideoCapture(1)
+
+    frames = []
+    
+    image_count = 0
+    frames += get_images('img')
+    # 2. Save GIF animation
+    print("Saving GIF file")
+    with imageio.get_writer("output.gif", mode="I", duration=0.2) as writer:
+        print('writing inside file...')
+        for idx, frame in enumerate(frames):
+            print("Adding frame to GIF file: ", idx + 1)
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            writer.append_data(rgb_frame)
+        print("gif generated")
+
+def main():
+    generate_gif()
+
+        
+if __name__=='__main__':
+    main()
